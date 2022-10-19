6e822f080b89382c97223c61e748ad59f06ade26
J-Sub <james319@naver.com>
2022-10-19 14:34:03 +0900

    matrix, fix SDAE embedding

--- data/deepDTnet/SDAE.py
+++ data/deepDTnet/SDAE.py
@@ -20,7 +20,7 @@ args = parser.parse_args()
 EPOCH = 10
 BATCH_SIZE = args.batch_size
 USE_CUDA = torch.cuda.is_available()
-print(USE_CUDA, ' use cuda')
+print(USE_CUDA, 'use cuda')
 # num_cuda = 0
 # os.environ['CUDA_VISIBLE_DEVICES'] = str(num_cuda) 
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else 'cpu')
@@ -47,20 +47,22 @@ class MyDataset(object):
   def __init__(self,file_name):
     ppmi_df=pd.read_csv(file_name, sep='\t', header=0)
     x=ppmi_df.iloc[:,1:].values
-    self.x_train=torch.tensor(x,dtype=torch.float32)
-
+    self.x_train=torch.tensor(x,dtype=torch.float32) #! (732,732)
+    
+    
   def __len__(self):
     return self.x_train.shape[0]
    
   def __getitem__(self,idx):
     return self.x_train[idx]
 
+# txt_path = "./PPMI_matrix.txt"
 matrix_dir = args.matrix_dir
 trainset = MyDataset(matrix_dir)
 train_loader = torch.utils.data.DataLoader(
     dataset     = trainset,
     batch_size  = BATCH_SIZE,
-    shuffle     = True,
+    shuffle     = False,
     num_workers = 2
 )
 
@@ -101,7 +103,7 @@ class Autoencoder(nn.Module):
                 nn.Linear(64, 128),
                 nn.ReLU(),
                 nn.Linear(128, 732),
-                nn.Sigmoid(),       # 픽셀당 0과 1 사이로 값을 출력
+                nn.Sigmoid(),       # 픽셀당 0과 1 사이로 값을 출력합니다
             )
         else:
             raise Exception("hidden_dim is not 3 or 12")
@@ -127,30 +129,33 @@ def add_noise(img):
 def train(autoencoder, train_loader):
     autoencoder.train()
     avg_loss = 0
+    encoder_list = []
     for step, x in enumerate(train_loader):
         
         noisy_x = add_noise(x)  # 입력에 노이즈 더하기
         noisy_x = noisy_x.to(DEVICE)
+        
         y = x.to(DEVICE)
 
         encoded, decoded = autoencoder(noisy_x)
-
+        
         loss = criterion(decoded, y)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
         
         avg_loss += loss.item()
-    return avg_loss / len(train_loader), encoded
+        encoder_list.append(encoded.cpu().detach().numpy())
+    return avg_loss / len(train_loader), encoder_list
 
 embed = []
 for epoch in range(1, EPOCH+1):
-    loss, embedding = train(autoencoder, train_loader)
-    embed.append(embedding.cpu().detach().numpy())
+    loss, encoder_list = train(autoencoder, train_loader)
+    if epoch == EPOCH:
+        encoder_list = np.concatenate(encoder_list, axis=0)
+        embeddf = pd.DataFrame(encoder_list)
     print("[Epoch {}] loss:{}".format(epoch, loss))
-    
-embed = np.concatenate(embed, axis=0)
-embeddf = pd.DataFrame(embed)
+
 checkmkdir('./embed')
 save_embed_dir = './embed/embed_'+matrix_dir[9:-4]+'_'+str(hidden_dim)+'D.csv'
 embeddf.to_csv(save_embed_dir, index=False)
